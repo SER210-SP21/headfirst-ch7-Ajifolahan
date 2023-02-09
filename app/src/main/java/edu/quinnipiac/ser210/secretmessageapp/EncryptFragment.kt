e76ae5a5005a2379d34674baa1d8931a7fc09c69
Momoreoluwa Ayinde <maayinde@quinnipiac.edu>
2023-02-09 13:06:16 -0500

    Initial Commit

--- app/src/main/java/edu/quinnipiac/ser210/secretmessageapp/EncryptFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/secretmessageapp/EncryptFragment.kt
@@ -1,19 +1,28 @@
 package edu.quinnipiac.ser210.secretmessageapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.findNavController
 
 class EncryptFragment : Fragment() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        //from bundle() retrieves any argument passed to the fragment.
+        val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
+        val encryptedView = view.findViewById<TextView>(R.id.encrypted_message)
+        encryptedView.text = message.reversed()
+        return view
     }
 
 
--- app/src/main/java/edu/quinnipiac/ser210/secretmessageapp/MessageFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/secretmessageapp/MessageFragment.kt
@@ -1,19 +1,35 @@
-//package edu.quinnipiac.ser210.secretmessageapp
-//
-//import android.os.Bundle
-//import androidx.fragment.app.Fragment
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//
-//
-//class MessageFragment : Fragment() {
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_message, container, false)
-//    }
-//
-//}
+package edu.quinnipiac.ser210.secretmessageapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.findNavController
+
+
+class MessageFragment : Fragment() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        //uses the root view to get a reference to the fragment's start button
+        val nextButton = view.findViewById<Button>(R.id.next)
+        val messageView = view.findViewById<EditText>(R.id.message)
+        //on click, do this.
+        nextButton.setOnClickListener {
+            val message = messageView.text.toString()
+            val action = MessageFragmentDirections.actionMessageFragmentToEncryptFragment(message)
+            view.findNavController().navigate(action)
+        }
+
+        return view
+    }
+
+}
